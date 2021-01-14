b4da3bc87b99a5555ae17d0cdcf2f64a04271260
sssukho <sssukho@gmail.com>
2021-01-14 23:33:07 +0900

    리다이렉팅 - 6.4 Redirecting

--- movie_app/src/routes/Detail.js
+++ movie_app/src/routes/Detail.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-function Detail({ location }) {
-    console.log(location);
-    return <span>hello</span>;
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        // state가 undefined면 유저를 데려와서 홈으로 redirecting 시켜버릴 것임.
+        if(location.state === undefined) {
+            history.push("/"); 
+        }
+    }
+    render() {
+        const { location } = this.props;
+        return <span>{location.state.title}</span>;
+
+    }
 }
 
 export default Detail;
